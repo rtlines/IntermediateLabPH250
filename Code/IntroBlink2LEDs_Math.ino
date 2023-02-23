df1dadcb0e0cf720818707b605ab363c6399563a
rtlines <48268649+rtlines@users.noreply.github.com>
2023-02-23 11:43:49 -0700

    emergency fix for first chapter, wrong code was included for double blink with math

--- Code/IntroBlink2LEDs_Math.ino
+++ Code/IntroBlink2LEDs_Math.ino
@@ -3,14 +3,9 @@
 // to determine when they should light. Note that the
 // Arduino code is closer to C++ than python.
 /////////////////////////////////////////////////////////
-int ledPin1=13;
-int ledPin2=12;
+int ledPin1=7;
+int ledPin2=8;
 int i=0; //loop counter
- 
-/////////////////////////////////////////////////////////
-int fib_count=0; // number of blinks based on Fibonacci
-int i_max=10; // maximum Fibonacci number before 
- // starting over
  
 /////////////////////////////////////////////////////////
 void setup() {
@@ -19,48 +14,23 @@ void setup() {
  pinMode(ledPin2, OUTPUT);
  } 
  
-int fib(int x) {
- // calculates the Fibonacci sequence using recursion
- if (x==0) 
-    return 0;
- if (x==1)
-   return 1;
-   return fib(x-1) + fib(x-2);
- } 
- 
 /////////////////////////////////////////////////////////
 void loop() {
  // put your main code here, to run repeatedly:
  // blink the LED's with the number of blinks being 
  // the Fibonacci sequence.
- fib_count=fib(i);
  if (i % 2 == 0 ) {
-   // turn off one light
-   digitalWrite(ledPin2,LOW);
-   // now blink the second light fib_count times 
-   for (int n=0; n<fib_count; n++) { 
-      digitalWrite(ledPin1,HIGH);
-      delay(100);
-      digitalWrite(ledPin1,LOW);
-      delay(100);
-   }
+    digitalWrite(ledPin1,HIGH);
+    digitalWrite(ledPin2,LOW);
+    delay(1000);
+    }
+  else {
+    digitalWrite(ledPin1,LOW);
+    digitalWrite(ledPin2,HIGH);
+    delay(1000);
+    }
+    i++;   // increment the loop counter by adding 1 to i
  }
- else {
-   // turn off the other light
-   digitalWrite(ledPin1,LOW);
-   // now blink the first light fib_count times
-   for (int n=0; n<fib_count ; n++) { 
-      digitalWrite(ledPin2,HIGH);
-      delay(100);
-      digitalWrite(ledPin2,LOW);
-      delay(100);
-   }
- }
- // increment i
- i++;
- // limit our blinks to the first i_max Fibonacci numbers
- if (i>i_max) i=0;
- }
-}
+
 /////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////
